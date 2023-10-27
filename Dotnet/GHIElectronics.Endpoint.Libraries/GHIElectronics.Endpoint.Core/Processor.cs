b83d6c22acdcecd2692cd55b6af9bd6e020d9b6c
Dat Tran <dat.tran@ghielectronics.com>
2023-10-27 14:49:20 -0400

    Add Native class

--- Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Core/Processor.cs
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Core/Processor.cs
@@ -6,8 +6,7 @@ using System.Threading.Tasks;
 
 namespace GHIElectronics.Endpoint.Core {
     public static class Processor {
-
-        public const string AM335x = "AM335x";
+        
         public const string STM32MP1 = "STM32MP1";
 
         public static string Name { get; internal set; } = STM32MP1;
---
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Native/DeviceInformation.cs
@@ -0,0 +1,51 @@
+using GHIElectronics.Endpoint.Core;
+
+namespace GHIElectronics.Endpoint.Native {
+
+    public static class Power {
+        public static void Reboot() {
+            var script = new Script("reboot", "./", "");
+            script.Start();
+        }
+    }
+    public static class DeviceInformation {
+
+        public static string Version => GetVersion();
+        private static string GetVersion() {
+
+
+            var script = new Script("get_version.sh", "./", "");
+            script.Start();
+
+            var version = script.Output.Replace("\n", "");
+            return version;
+
+        }
+
+        public static void EnableAutoRun(string applicationName) {
+            var application_fullpath = "/root/.epdata/" + applicationName + "/" + applicationName + ".dll";
+            
+                var script = new Script("csharp_generate_startup.sh", "./", "1 /etc/init.d/S90custom " + application_fullpath);
+
+                script.Start();                                                
+        }
+
+        public static void DisableAutoRun() {
+            var script = new Script("csharp_generate_startup.sh", "./", "0 /etc/init.d/S90custom");
+
+            script.Start();
+        }
+
+        public static void DisableSSH() {
+
+            var script = new Script("rm", "./", "/etc/init.d/S50dropbear");
+
+            script.Start();
+
+            script = new Script("rm", "./", "/etc/init.d/S30usbgadget");
+
+            script.Start();
+
+        }
+    }
+}
