1f993432581690453c063b2dfe02254763df6101
Dat Tran <dat.tran@ghielectronics.com>
2023-12-28 15:12:42 -0500

    clean

--- Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Core/Utils.cs
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Core/Utils.cs
@@ -34,30 +34,6 @@ namespace GHIElectronics.Endpoint.Core {
 
         [DllImport(LibNativeUtils)]
         internal static extern int native_wfi();
-        //public static string GetUntil(this string that, char @char) {
-        //    return that[..(IndexOf() == -1 ? that.Length : IndexOf())];
-        //    int IndexOf() => that.IndexOf(@char);
-        //}
-
-        //public static string FindAndSplitUntil(string str, string substr, char until) {
-        //    var pos_start = str.IndexOf(substr);
-
-        //    if (pos_start == -1) {
-        //        return string.Empty;
-        //    }
-
-        //    var pos_end = pos_start + substr.Length;
-
-        //    while (pos_end < str.Length) {
-        //        if (str[pos_end] == until) {
-        //            break;
-        //        }
-
-        //        pos_end++;
-        //    }
-
-        //    return str.Substring(pos_start, pos_end - pos_start);
-
-        //}
+        
     }
 }
