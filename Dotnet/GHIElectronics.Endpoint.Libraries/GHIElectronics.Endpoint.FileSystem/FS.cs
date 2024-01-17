94b80470842c17efdc61e1a0cfe90e1a9013d764
Dat Tran <dat.tran@ghielectronics.com>
2024-01-17 10:45:31 -0500

    use GUI

--- Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.FileSystem/FS.cs
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.FileSystem/FS.cs
@@ -4,35 +4,15 @@ using GHIElectronics.Endpoint.Core;
 namespace GHIElectronics.Endpoint {
     public static class FileSystem {
 
-        //public static string Mount(string deviceName, string mountedName = "") => Mount(deviceName, -1, mountedName);
-
-        //public static string Mount(string deviceName, int deviceId) => Mount(deviceName, deviceId, "");
-
-        const string ROOT_FS_FOLDER = "/media/.fsmount";
+        const string ROOT_FS_FOLDER = "/media";
         public static string Mount(string deviceName) {
-
-            var rnd = new Random();
-            var num = rnd.Next();
             Script script;
-
-
             string arg;
-            if (!Directory.Exists(ROOT_FS_FOLDER)) {
-
-                arg = string.Format("{0}", ROOT_FS_FOLDER);
-                script = new Script("mkdir", "./", arg);
-
-                script.Start();
-
-                if (script.Error.Length > 0) {
-                    throw new Exception(script.Error);
-                }
-            }
 
             if (deviceName == null || deviceName.Length == 0)
                 throw new Exception("Invalid argument");
 
-            var folder_name = string.Format("media{0}", num);
+            var folder_name = Guid.NewGuid().ToString("d");
 
            
 
@@ -64,10 +44,6 @@ namespace GHIElectronics.Endpoint {
             throw new Exception("Could not mount the device " + folder_name);
 
         }
-
-        //public static void Unmount(int deviceId) => Unmount(deviceId, "");
-
-        //public static void Unmount(string specialName) => Unmount(-1, specialName);
         public static void Unmount(string path) {
 
             if (path == null || path.Length == 0)
