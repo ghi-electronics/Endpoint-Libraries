2c47ea489d7d4b4d71924ed31e5728a6e87746db
greg-norris <greg.norris@ghielectronics.com>
2024-01-22 05:43:50 -0500

    Fix conflict in Filesystem

--- Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.FileSystem/FS.cs
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.FileSystem/FS.cs
@@ -3,7 +3,6 @@ using GHIElectronics.Endpoint.Core;
 
 namespace GHIElectronics.Endpoint {
 
-
     /**<example>
     Write file to USB
     <code>
@@ -19,16 +18,91 @@ namespace GHIElectronics.Endpoint {
 
     public static class FileSystem {
 
+        const string ROOT_FS_FOLDER = "/media";
+        public static string Mount(string deviceName) {
+            Script script;
+            string arg;
+
+            if (deviceName == null || deviceName.Length == 0)
+                throw new Exception("Invalid argument");
+
+            var folder_name = Guid.NewGuid().ToString("d");
+
+
+
+            var dir = ROOT_FS_FOLDER + "/" + folder_name;
+
+            if (Directory.Exists(dir)) {
+                throw new Exception("The directory is already exist. Use different mountedName");
+            }
+
+            if (!Directory.Exists(dir)) {
+                arg = string.Format("{0}", dir);
+                script = new Script("mkdir", "./", arg);
+
+                script.Start();
+            }
+
+            if (Directory.Exists(dir)) {
+
+                arg = string.Format("{0} {1}", deviceName, dir);
+
+                script = new Script("mount", "./", arg);
+
+                script.Start();
+
+                if (script.ExitCode == 0)
+                    return dir;
+
+            }
+            throw new Exception("Could not mount the device " + folder_name);
+
+        }
+        public static void Unmount(string path) {
+
+            if (path == null || path.Length == 0)
+                throw new Exception("Could not found the device for unmount");
+
+            var dir = path;
+
+            var script = new Script("umount", "./", dir);
+
+            script.Start();
+
+            if (script.ExitCode == 0) {
+
+                var arg = string.Format("-r {0}", dir);
+
+                script = new Script("rm", "./", arg);
+
+                script.Start();
+
+                if (script.ExitCode == 0) {
+                    return;
+                }
+            }
+
+            throw new Exception("Could not umount the device " + dir);
+
+
+        }
+
+        public static void Flush() {
+            var script = new Script("sync", "./", "");
+
+            script.Start();
+        }
+    }
+}
 
 
 
 
+//public static string Mount(string deviceName, string mountedName = "") => Mount(deviceName, -1, mountedName);
 
-        //public static string Mount(string deviceName, string mountedName = "") => Mount(deviceName, -1, mountedName);
+//public static string Mount(string deviceName, int deviceId) => Mount(deviceName, deviceId, "");
 
-        //public static string Mount(string deviceName, int deviceId) => Mount(deviceName, deviceId, "");
-
-        const string ROOT_FS_FOLDER = "/media/.fsmount";
+const string ROOT_FS_FOLDER = "/media/.fsmount";
         public static string Mount(string deviceName) {
 
             var rnd = new Random();
