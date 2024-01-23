fd1c11c4c9d8287b35c84bf5bb3f02c363dfef97
Dat Tran <dat.tran@ghielectronics.com>
2024-01-23 10:36:34 -0500

    clean older files

--- Dotnet/GHIElectronics.Endpoint.Libraries/dopacknuget.bat
+++ Dotnet/GHIElectronics.Endpoint.Libraries/dopacknuget.bat
@@ -7,7 +7,9 @@ SET CurrentDir=%CD%
 IF "%DoAssemblySign%" == "true" (
 	pushd "%CurrentDir%"
 
-	DEL /Q *.nupkg
+	IF EXIST "..\output\%OutputAssemblyName%*.nupkg" (
+		DEL /Q ..\output\%OutputAssemblyName%*.nupkg
+	)
 
 	signtool.exe sign /fd sha512 /f "%VsixSignerCertificatePath%" /p "%VsixSignerCertificatePassword%" /t "http://timestamp.digicert.com" /sha1 "%AssemblySignerCertificateSha1%" "%OutputLocation%\%OutputAssemblyName%.dll"
 	nuget pack "%OutputAssemblyName%.nuspec" -Properties Configuration=%BuildMode% -OutputDirectory "..\output"
