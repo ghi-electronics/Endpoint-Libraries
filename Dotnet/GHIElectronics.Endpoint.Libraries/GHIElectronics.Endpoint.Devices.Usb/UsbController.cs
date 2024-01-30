06c9d51d634d2d29fdbca50ca8219319c320e885
Dat Tran <dat.tran@ghielectronics.com>
2024-01-30 10:07:49 -0500

    remove unused enum

--- Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Devices.Usb/UsbController.cs
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Devices.Usb/UsbController.cs
@@ -12,14 +12,14 @@ namespace GHIElectronics.Endpoint.Devices.Usb
         /// <summary>Human Interface Device.</summary>
         HID,
 
-        /// <summary>Mouse.</summary>
-        Mouse,
+        ///// <summary>Mouse.</summary>
+        //Mouse,
 
-        /// <summary>Keyboard.</summary>
-        Keyboard,
+        ///// <summary>Keyboard.</summary>
+        //Keyboard,
 
-        /// <summary>Joystick.</summary>
-        Joystick,
+        ///// <summary>Joystick.</summary>
+        //Joystick,
 
         /// <summary>Mass Storage. This includes USB storage devices such as USB Thumbs drives and USB hard disks.</summary>
         MassStorage,
@@ -27,44 +27,40 @@ namespace GHIElectronics.Endpoint.Devices.Usb
         /// <summary>Webcam.</summary>
         Webcam,
 
-        /// <summary>Printer.</summary>
-        [Obsolete()]
-        Printer,
+        ///// <summary>Printer.</summary>
+        //[Obsolete()]
+        //Printer,
 
-        /// <summary>USB to Serial device.</summary>
-        SerialFTDI,
+        ///// <summary>USB to Serial device.</summary>
+        //SerialFTDI,
 
-        /// <summary>USB to Serial device.</summary>
-        [Obsolete()]
-        SerialProlific,
+        ///// <summary>USB to Serial device.</summary>
+        //[Obsolete()]
+        //SerialProlific,
 
-        /// <summary>USB to Serial device.</summary>
-        [Obsolete()]
-        SerialProlific2,
+        ///// <summary>USB to Serial device.</summary>
+        //[Obsolete()]
+        //SerialProlific2,
 
-        /// <summary>USB to Serial device.</summary>
-        [Obsolete()]
-        SerialSiLabs,
+        ///// <summary>USB to Serial device.</summary>
+        //[Obsolete()]
+        //SerialSiLabs,
 
-        /// <summary>USB to Serial device.</summary>
-        SerialCDC,
+        ///// <summary>USB to Serial device.</summary>
+        //SerialCDC,
 
-        /// <summary>USB to Serial device.</summary>
-        [Obsolete()]
-        SerialSierraC885,
+        ///// <summary>USB to Serial device.</summary>
+        //[Obsolete()]
+        //SerialSierraC885,
 
-        /// <summary>Sierra Installer.</summary>
-        [Obsolete()]
-        SierraInstaller,
+        ///// <summary>Sierra Installer.</summary>
+        //[Obsolete()]
+        //SierraInstaller,
 
         /// <summary>Video device.</summary>
         //Video,
 
         /// <summary>Webcamera.</summary>
         //Webcam,
-    }
-    public class UsbController
-    {
-
     }
 }
