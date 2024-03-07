3a8cf230bb564d74c40650b64b72e061362a1117
Dat Tran <dat.tran@ghielectronics.com>
2024-03-07 10:32:39 -0500

    update python setup

--- python/GHIElectronics.Endpoint.Core/setup.py
+++ python/GHIElectronics.Endpoint.Core/setup.py
@@ -4,16 +4,16 @@ import setuptools
 
 setuptools.setup(
     name = "GHIElectronics.Endpoint.Core",
-    version = "0.0.1",
+    version = "0.1.1",
     author = "GHI Electronics",
     author_email = "support@ghielectronics.com",
     license='MIT', 
-    description = "GHI Electronics DUE Python library.",
-    url = "https://www.duelink.com/",
+    description = "GHI Electronics Endpoint Python library.",
+    url = "https://www.ghielectronics.com/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",        
     ],
  
-    python_requires='>=3.6'
+    python_requires='>=3.10'
 )
