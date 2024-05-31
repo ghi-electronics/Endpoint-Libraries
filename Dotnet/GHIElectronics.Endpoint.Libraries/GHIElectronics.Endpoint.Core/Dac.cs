e227a27dd306bb96b6472e3b6b8fd675e4553c1e
Dat Tran <dat.tran@ghielectronics.com>
2024-05-31 13:17:37 -0400

    add missing dac

---
+++ Dotnet/GHIElectronics.Endpoint.Libraries/GHIElectronics.Endpoint.Core/Dac.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GHIElectronics.Endpoint.Core {
+    public static partial class EPM815 {
+
+
+        public static class Dac {
+
+            public static class Pin {
+                /// <summary>Adc pin.</summary>               
+                public const int PA5 = 5;
+            }
+
+            
+
+        }
+    }
+}
